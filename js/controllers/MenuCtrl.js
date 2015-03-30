48a72a8f53855d40b72897125f3fff14dc2ee03e
tomadj <thomas.adjadj@outlook.com>
2015-03-30 22:57:39 +0200

    ionic sidemenu with archi

---
+++ js/controllers/MenuCtrl.js
@@ -0,0 +1,7 @@
+app.controller('MenuCtrl', function($scope) {
+
+
+	
+	
+	
+});
---
+++ js/router.js
@@ -0,0 +1,33 @@
+var router = app.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'MenuCtrl'
+  })
+
+  .state('app.home', {
+    url: "/home",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/home.html"
+         controller: 'HomeCtrl'
+      }
+    }
+  })
+
+  .state('app.about', {
+    url: "/about",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/about.html"
+         controller: 'AboutCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home');
+});
